5c48e78b740e1ec883945b9ed201cd5840fa7361
misschildhood <923219711@qq.com>
2020-03-13 00:14:07 +0800

    完成日志统计任务类

--- Project2_日志统计/src/main/java/com/goddong/log_analysis/MR_logAnalysis.java
+++ Project2_日志统计/src/main/java/com/goddong/log_analysis/MR_logAnalysis.java
@@ -2,6 +2,12 @@ package com.goddong.log_analysis;
 
 
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -18,6 +24,23 @@ public class MR_logAnalysis extends Configured implements Tool {
 
 
     public int run(String[] strings) throws Exception {
-        return 0;
+        Job job = Job.getInstance(getConf(), "WLA_analysis");
+        String inputPath = "./baidu.log";
+        String outPutPath = "./result.log";
+        Path path = new Path(outPutPath);
+        path.getFileSystem(getConf()).delete(path, true);
+
+        //远程运行要加上这一句
+        //        job.setJarByClass(MR_logAnalysis.class);
+        FileInputFormat.setInputPaths(job, inputPath);
+        job.setMapperClass(WLA_Mapper.class);
+        job.setMapOutputKeyClass(LongWritable.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setReducerClass(WLA_Reducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        FileOutputFormat.setOutputPath(job, path);
+
+        return job.waitForCompletion(true) ? 0 : 1;
     }
 }
